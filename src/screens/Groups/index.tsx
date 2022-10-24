06c98b7bb9b905985e3edac0348709201470cc06
Tfranch8 <tfranch8@its.jnj.com>
2022-10-24 17:01:50 -0300

    Button Create

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,37 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
-import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header showBackButton />
       <Highlight title="Team" subtitle='Play with your friends'/>
-      <GroupCard title="Rapaziada Gamer" />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message="Cadastre a parada ai fera..."/>}
+      />
+      
+      <Button 
+        title='Criar nova turma'
+      />
     </Container>
   );
 }
